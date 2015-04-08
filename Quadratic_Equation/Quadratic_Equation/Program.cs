8c545b830bf9aaba1eac6406415c0e2c2195642f
ATrzeciakiewicz <a.trzeciakiewicz@hotmail.com>
2015-04-08 21:20:23 +0200

    Add classes, methods, struct and constructor.

--- Quadratic_Equation/Quadratic_Equation/Program.cs
+++ Quadratic_Equation/Quadratic_Equation/Program.cs
@@ -3,43 +3,25 @@
 
 namespace Quadratic_Equation
 {
+    public interface IQuadraticEquation
+        {
+        void Calculate();
+        }
     class Program
-    {
+    {       
         static void Main(string[] args)
         {
-            double a, b, c, delta, result;
-            a = 1;
-            b = -5;
-            c = 4;
-            delta = (Math.Pow(b,2) - (4 * a * c));
-
-            Console.WriteLine("Parameters quadratic equation:");
-            Console.WriteLine("A = " + a + ", B = " + b + ", C = " + c);
-            Console.WriteLine("Discriminant = " + delta); 
-
-            if (a==0)
-            {
-                Console.WriteLine("This is not quadratic equation, because A=0!");
-            }
-              else if (delta<0)
-               {
-                Console.WriteLine("If the discriminant is negative, then there are no real roots");
-               }
-              else if (delta == 0)
-               {                 
-                result = -b / (2 * a);
-                Console.WriteLine("Solution: x = " + result);               
-               }
-            else
-              {
-               result = (-b + Math.Sqrt(delta)) /( 2 * a);
-               Console.Write("Solution: x1 = " + result);
-               result = (-b - Math.Sqrt(delta)) / (2 * a);
-               Console.WriteLine(", x2 = " + result);
-              }
-            Console.ReadLine();
-            }
+            QuadraticEquation myEquation = new QuadraticEquation();
             
-        }
-    }
+            Console.WriteLine("Parameters quadratic equation:");
+            Console.WriteLine("A = " + myEquation.a + ", B = " + myEquation.b + ", C = " + myEquation.c);
+
+            //wywołanie metody
+            myEquation.Validate();
+
+            Console.ReadLine();
+         }
+     }                  
+}
+
 
---
+++ Quadratic_Equation/Quadratic_Equation/QuadraticEquation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadratic_Equation
+{
+    class QuadraticEquation : IQuadraticEquation
+    {
+        public double a;
+        public double b;
+        public double c;
+     
+        //konstruktor
+      public QuadraticEquation()
+        {
+            a = 0;
+            b = -5;
+            c = 4;
+        }
+      //metoda
+       public void Validate()
+      {
+          if (a == 0)
+          {
+              Console.WriteLine("This is not quadratic equation, because A=0!");
+          }
+           else
+          {
+              Calculate();
+          }
+          
+      }
+       //metoda
+      public void Calculate()
+      {
+          
+          EquationResult myResult = new EquationResult();
+          myResult.Delta = (Math.Pow(b, 2) - (4 * a * c));          
+          myResult.X = -b / (2 * a);
+          myResult.X1 = (-b + Math.Sqrt(myResult.Delta)) / (2 * a);
+          myResult.X2 = (-b - Math.Sqrt(myResult.Delta)) / (2 * a);
+   
+          
+            if (myResult.Delta < 0)
+            {
+                Console.WriteLine("Discriminant = " + myResult.Delta);
+                Console.WriteLine("If the discriminant is negative, then there are no real roots");
+            }
+            else if (myResult.Delta == 0)
+            {
+                Console.WriteLine("Discriminant = " + myResult.Delta);
+                Console.WriteLine("Solution: x = " + myResult.X);
+            }
+            else
+            {
+                Console.WriteLine("Discriminant = " + myResult.Delta);
+                Console.Write("Solution: x1 = " + myResult.X1 + ", x2 = " + myResult.X2);
+            }
+      }
+    }
+
+    struct EquationResult
+    {
+        public double X { get; set; }
+        public double X1 { get; set; }
+        public double X2 { get; set; }
+        public double Delta { get; set; }       
+    }
+}
